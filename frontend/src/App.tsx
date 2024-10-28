d191476a9e204af11d676618fdb372f1b99a32f3
ishida530 <ishida530@gmail.com>
2024-10-28 20:00:26 +0100

    use env variables and handle mailer while register

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,25 @@
 import React, { useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom'; // Import useLocation
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"; // Import SidebarProvider
-import { AppSidebar } from './components/common/AppSidebar'; // Import AppSidebar
+import { Outlet, useLocation } from 'react-router-dom';
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"; 
+import { AppSidebar } from './components/common/AppSidebar'; 
 
 
 const App: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false); // State to manage sidebar visibility
-  const location = useLocation(); // Get the current location
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
 
-  // Check if the current path is '/login'
   const isLoginPage = location.pathname === '/login';
 
   return (
     <SidebarProvider>
       <div className={`flex h-screen relative ${isLoginPage ? 'w-full' : ''}`}>
-        {/* Only show SidebarTrigger if not on the login page */}
         {!isLoginPage && (
           <>
             <SidebarTrigger
               onClick={() => setIsOpen(!isOpen)}
               className="p-4 md:hidden absolute"
             >
-              ☰ {/* Menu icon */}
+          
             </SidebarTrigger>
             <AppSidebar />
           </>
